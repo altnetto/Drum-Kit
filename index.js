e54868897844d8e6ebe1cd165b74a6ab7622b73c
Altieres Schincariol <69064167+altnetto@users.noreply.github.com>
2020-12-02 14:46:17 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+window.addEventListener('keydown', playSound);
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => {
+    key.addEventListener('transitionend', removeTransition);
+});
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+function playSound(e) {
+
+    const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+    const key = document.querySelector(`.key[data-key="${e.key}"]`);
+
+
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
